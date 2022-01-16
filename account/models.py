73c1351c86f1228291ab57d0b3f6338a354657e0
beibars <mukashov.beibars2003@gmail.com>
2022-01-16 19:49:28 +0600

    Registration&Authorization&Activate&Login&Logout

---
+++ account/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class MyUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self,email,password,**extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.create_activation_code()
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
+    activation_code = models.CharField(max_length=50,blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def create_activation_code(self):
+        import hashlib
+        string = self.email + str(self.id)
+        encode_string = string.encode()
+        md5_object = hashlib.md5(encode_string)
+        activation_code = md5_object.hexdigest()
+        self.activation_code = activation_code
+
+
+# Gender = (
+#     ('female', 'female'),
+#     ('male', 'male'),
+#     ('other','other'),
+# )
+
+
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(MyUser, primary_key=True, related_name='profiles', on_delete=models.CASCADE)
+#     name = models.CharField(max_length=100, blank=True, null=True)
+#     gender = models.CharField(choices=Gender)
+#     date_of_birth = models.DateField(null=True, blank=True)
+#     avatar = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+#     bio = models.CharField(max_length=250, blank=True, null=True)
---
+++ account/serializers.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import authenticate
+from rest_framework import serializers
+
+from account.models import MyUser
+from account.utils import send_activation_code
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(min_length=6,write_only=True)
+    password_confirm = serializers.CharField(min_length=6,write_only=True)
+
+    class Meta:
+        model = MyUser
+        fields = ('email','password','password_confirm')
+
+    def validate(self,validated_data):
+        password = validated_data.get('password')
+        password_confirm = validated_data.get('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError('Пароли не совпадают')
+        return validated_data
+
+    def create(self, validated_data):
+        email = validated_data.get('email')
+        password = validated_data.get('password')
+        user = MyUser.objects.create_user(email=email,password=password)
+        send_activation_code(email=user.email,activation_code=user.activation_code)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(label='Password',style={'input_type': 'password'},trim_whitespace=False)
+
+    def validate(self,attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            user = authenticate(request=self.context.get('request'),email=email,password=password)
+            if not user:
+                message = 'Невозможно войти с предоставленными учетными данными'
+                raise serializers.ValidationError(message,code='authorization')
+        else:
+            message = 'Должен включать "email" и "пароль".'
+            raise serializers.ValidationError(message,code='authorization')
+
+        attrs['user'] = user
+        return attrs
